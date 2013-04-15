5b5d2d0e529562d8781b4d216dada07604a6fd2b
Vairaa Nicolas <vairaa.nicolas@gmail.com>
2013-04-15 16:06:56 +0200

    texte dans editeur

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask.ext.login import (LoginManager, current_user, login_required, login_user, logout_user, UserMixin, AnonymousUser, confirm_login, fresh_login_required)
-import function
+import function, sys
 
 app = Flask(__name__)
 
@@ -21,16 +21,21 @@ def home():
   return render_template('home.html')
 
 @app.template_filter('get_xml')
-def get_xml(ficxml):
-	ofi = open(ficxml, 'r')
-	ficxml = ofi.read()
-	return ficxml
+def get_xml(xml):
+	ofi = open(xml, 'r')
+	xml = ofi.read()
+	xml = xml.decode('utf-8')
+	return xml
 
 @app.route('/test')
 def test():
 	packs,shortname, i, pack = function.get_packages()
 	grps = function.get_group()
 	return render_template('test.html', grps=grps, packs=packs, shortname=shortname, i=i, pack=pack)
+
+def test():
+    b = request.args.get(b)
+    return jsonify(result=b|get_xml)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
