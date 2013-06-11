77c45d7c0a6109bd07d9744f5c23fbf5dedfe8e5
Christophe DEZE <cdeze@ac-nantes.fr>
2013-06-11 17:00:40 +0200

    ajout fichier de setup.py skeletor

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    author='Nicolas Vairaa Christophe DEZE',
+    author_email='cdeze@ac-nantes.fr',
+    name='python-<APPLICATION>',
+    version='2.3.0',
+    description='scribepkg',
+    url='http://www.eole.orion.education.fr',
+    package_dir={'':'src'},  # root
+    packages=['scribepkg'],
+)
+
